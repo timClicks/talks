941d5045462eec1ddd92e98ad097e08f17376396
Tim McNamara <code@timmcnamara.co.nz>
2024-11-07 21:02:37 +1300

    remove target dir

--- 202411-rustlab-webapi-workshop/src/exercises/checksecrets/target/debug/build/proc-macro2-2d2907554eb5faf1/build_script_build-2d2907554eb5faf1.d
+++
@@ -1,5 +0,0 @@
-/home/tim/Talks/talks/202411-rustlab-webapi-workshop/src/exercises/keyscanner/target/debug/build/proc-macro2-2d2907554eb5faf1/build_script_build-2d2907554eb5faf1: /home/tim/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.89/build.rs
-
-/home/tim/Talks/talks/202411-rustlab-webapi-workshop/src/exercises/keyscanner/target/debug/build/proc-macro2-2d2907554eb5faf1/build_script_build-2d2907554eb5faf1.d: /home/tim/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.89/build.rs
-
-/home/tim/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.89/build.rs:
--- 202411-rustlab-webapi-workshop/src/exercises/checksecrets/target/debug/deps/unicode_ident-c04c3c798f48f598.d
+++
@@ -1,8 +0,0 @@
-/home/tim/Talks/talks/202411-rustlab-webapi-workshop/src/exercises/keyscanner/target/debug/deps/libunicode_ident-c04c3c798f48f598.rmeta: /home/tim/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/lib.rs /home/tim/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/tables.rs
-
-/home/tim/Talks/talks/202411-rustlab-webapi-workshop/src/exercises/keyscanner/target/debug/deps/libunicode_ident-c04c3c798f48f598.rlib: /home/tim/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/lib.rs /home/tim/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/tables.rs
-
-/home/tim/Talks/talks/202411-rustlab-webapi-workshop/src/exercises/keyscanner/target/debug/deps/unicode_ident-c04c3c798f48f598.d: /home/tim/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/lib.rs /home/tim/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/tables.rs
-
-/home/tim/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/lib.rs:
-/home/tim/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.13/src/tables.rs:
